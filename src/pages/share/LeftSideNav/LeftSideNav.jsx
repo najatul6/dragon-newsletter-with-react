a1d18841c85652fbb8928afa6fffc9f860c8ce07
najatul6 <mdnajatulislam.develop@gmail.com>
2023-10-03 21:36:57 +0600

    complete left side and right side Nav section

--- src/pages/share/LeftSideNav/LeftSideNav.jsx
+++ src/pages/share/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left Side Nav</h2>
+        <div className="p-4">
+            <h2 className="text-2xl font-semibold">All Caterogy</h2>
+            <div className="mt-5">
+                {
+                    categories.map(category => <NavLink
+                        to={`/category/${category.id}`}
+                        className="block text-[#9F9F9F] text-xl font-semibold py-4 w-full pl-5 hover:bg-[#E7E7E7] rounded-lg"
+                        key={category.id}>
+                        {category.name}
+                    </NavLink>)
+                }
+            </div>
         </div>
     );
 };
--- src/pages/share/RightSideNav/RightSideNav.jsx
+++ src/pages/share/RightSideNav/RightSideNav.jsx
@@ -1,8 +1,62 @@
+import { AiOutlineGoogle, AiOutlineGithub } from "react-icons/ai";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import qZone1 from '../../../assets/qZone1.png'
+import qZone2 from '../../../assets/qZone2.png'
+import qZone3 from '../../../assets/qZone3.png'
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right side Nav</h2>
+            <div className="p-4">
+                <h2 className="text-xl font-semibold">Login With</h2>
+                <div className="space-y-2 mt-3">
+                    <button className="text-xl font-semibold flex items-center rounded-lg border w-full py-2 justify-center hover:text-[#3862b6] hover:border-[#3862b6]">
+                        <AiOutlineGoogle className="mr-1"></AiOutlineGoogle>
+                        Login with Google
+                    </button>
+                    <button className="text-xl font-semibold flex items-center rounded-lg border w-full py-2 justify-center hover:text-[#3862b6] hover:border-[#3862b6]">
+                        <AiOutlineGithub className="mr-1"></AiOutlineGithub>
+                        Login with Github
+                    </button>
+                </div>
+            </div>
+
+            {/* Find us */}
+            <div className="mt-7 p-4">
+                <h2 className="text-xl font-semibold">Find Us On</h2>
+                <div className="mt-5 rounded-lg border">
+                    <a href="" className="font-medium text-lg flex space-x-3 items-center p-4">
+                        <p className="text-[#3b599c] flex justify-center items-center w-8 h-8 p-1 bg-[#F3F3F3] rounded-full">
+                            <FaFacebookF></FaFacebookF>
+                        </p>
+                        <span className="text-[#706F6F] ">Facebook</span>
+                    </a>
+                    <hr />
+
+                    <a href="" className="font-medium text-lg flex space-x-3 items-center p-4">
+                        <p className="text-[#58a7de] flex justify-center items-center w-8 h-8 p-1 bg-[#F3F3F3] rounded-full">
+                            <FaTwitter></FaTwitter>
+                        </p>
+                        <span className="text-[#706F6F] ">Twitter</span>
+                    </a>
+                    <hr />
+
+                    <a href="" className="font-medium text-lg flex space-x-3 items-center p-4">
+                        <p className="text-[#E1306C] flex justify-center items-center w-8 h-8 text-center bg-[#F3F3F3] rounded-full"><FaInstagram></FaInstagram></p>
+                        <span className="text-[#706F6F] ">Instagram</span>
+                    </a>
+                </div>
+
+            </div>
+            {/* q zone */}
+            <div className="mt-5 p-4 bg-[#F3F3F3]">
+                <h2 className="text-xl font-semibold">Q-Zone</h2>
+                <div className="mt-5">
+                    <img src={qZone1} alt="Swimming image" />
+                    <img src={qZone2} alt="Class image" />
+                    <img src={qZone3} alt="Playground image" />
+                </div>
+            </div>
         </div>
     );
 };
